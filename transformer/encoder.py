57c6e278409191f3405abc395410f565e9a13d2b
Haebin Lim <fkid114@gmail.com>
2025-11-17 21:57:36 +0900

    add transformer encoder

--- transformer/encoder.py
+++ transformer/encoder.py
@@ -70,3 +70,96 @@ class MultiHeadAttention(nn.Module):
         output = self.linear_out(context) # final linear transformation
 
         return output
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self, emb_dim, max_len=5000):
+        super().__init__()
+
+        pe = torch.zeros(max_len, emb_dim)
+        position = torch.arange(0, max_len).unsqueeze(1)
+
+        dim_indices = torch.arange(0, emb_dim, 2).float()
+
+        div_term = 1.0 / (10000.0 ** (dim_indices / emb_dim)) # torch.exp(dim_indices * (-math.log(10000.0) / emb_dim))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)  # Shape: (1, max_len, emb_dim)
+
+        self.register_buffer('pe', pe) # non-trainable buffer
+
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1), :]
+    
+class PointwiseFeedForward(nn.Module):
+    def __init__(self, hidden_size, intermediate_size, dropout=0.1):
+        super().__init__()
+        self.fc1 = nn.Linear(hidden_size, intermediate_size)
+        self.fc2 = nn.Linear(intermediate_size, hidden_size)
+        self.activation = nn.ReLU() # nn.GELU()
+        self.dropout = nn.Dropout(dropout)
+        # self.norm = nn.LayerNorm(hidden_size)
+
+    def forward(self, x):
+        # residual = x
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.dropout(x)
+        # x = self.norm(residual + x) # residual connection with add & norm
+        return x
+    
+
+class EncoderLayer(nn.Module):
+    def __init__(self, hidden_size, intermediate_size, num_heads, dropout=0.1):
+        super().__init__()
+        self.mha = MultiHeadAttention(hidden_size, num_heads, dropout)
+        self.attn_norm = nn.LayerNorm(hidden_size)
+        self.dropout = nn.Dropout(dropout)
+
+        self.pff = PointwiseFeedForward(hidden_size, intermediate_size, dropout)
+        self.pff_norm = nn.LayerNorm(hidden_size)
+
+    def forward(self, x, pad_mask = None):
+        mha_output = self.mha(x, pad_mask)
+        mha_output = self.dropout(mha_output)
+        mha_output = self.attn_norm(x + mha_output)  # Add & Norm
+
+        x = self.pff(mha_output)
+        x = self.pff_norm(mha_output + x)  # Add & Norm  
+        return x
+        
+class Encoder(nn.Module):
+    def __init__(self, num_layers, hidden_size, intermediate_size, num_heads, dropout=0.1):
+        super().__init__()
+        self.layers = nn.ModuleList([
+            EncoderLayer(hidden_size, intermediate_size, num_heads, dropout)
+            for _ in range(num_layers)
+        ])
+
+    def forward(self, x, pad_mask = None):
+        for layer in self.layers:
+            x = layer(x, pad_mask)
+        return x
+    
+class TransformerEncoder(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.embedding = nn.Embedding(config.vocab_size, config.hidden_size)
+        self.pos_encoder = PositionalEncoding(config.hidden_size, config.max_len)
+        self.pos_dropout = nn.Dropout(config.dropout)
+        self.encoder = Encoder(
+            num_layers = config.num_layers,
+            hidden_size = config.hidden_size,
+            num_heads = config.num_heads,
+            intermediate_size = config.intermediate_size,
+            dropout = config.dropout
+        )
+    
+    def forward(self, input_ids, pad_mask = None):
+        x = self.embedding(input_ids)
+        x = self.pos_encoder(x)
+        x = self.pos_dropout(x)
+        x = self.encoder(x, pad_mask)
+        return x
